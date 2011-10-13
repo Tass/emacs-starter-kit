f2f1c4b6e7daaa10ae71198914cb54f1d0de820a
Simon Hafner <hafnersimon@gmail.com>
2011-10-13 16:15:33 +0200

    it's server-start not start-server

--- init.el
+++ init.el
@@ -97,5 +97,5 @@
 (defun detach-window () (interactive) (let ((new-frame (save-excursion(make-frame-command)))) (delete-window) (select-frame new-frame)))
 (global-set-key (kbd "C-x 2") 'detach-window)
 
-(start-server)
+(server-start)
 ;;; init.el ends here
