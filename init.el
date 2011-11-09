03c4004d0640b0673e1727643bc67c168bf8141d
Simon Hafner <hafnersimon@gmail.com>
2011-11-09 17:05:56 +0100

    clisp -> sbcl

--- init.el
+++ init.el
@@ -79,7 +79,7 @@
 (add-to-list 'load-path "~/.emacs.d/nimrod-mode")
 (require 'nimrod-mode)
 
-(setq inferior-lisp-program "/usr/bin/clisp")
+(setq inferior-lisp-program "/usr/bin/sbcl")
 (add-to-list 'load-path "/usr/share/emacs/site-lisp/slime/")
 (require 'slime)
 (slime-setup)
