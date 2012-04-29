e165d7b7fc267f7a29c59bd2ff177bf5662e3fca
Simon Hafner <hafnersimon@gmail.com>
2012-04-29 23:59:34 +0200

    custom variables

--- init.el
+++ init.el
@@ -144,3 +144,15 @@
 (load "auctex.el" nil t t)
 (load "preview-latex.el" nil t t)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(region ((t (:background "gold" :foreground "gtk_selection_fg_color")))))
