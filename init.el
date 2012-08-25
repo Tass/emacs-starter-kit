6e14c782ffdd8b2cab504db0feb568d98d19b0c5
Simon Hafner <hafnersimon@gmail.com>
2012-08-25 00:42:26 -0500

    fancier slime setup

--- init.el
+++ init.el
@@ -36,8 +36,8 @@
 
 (setq inferior-lisp-program "/usr/bin/sbcl")
 (add-to-list 'load-path "/usr/share/emacs/site-lisp/slime/")
-(require 'slime)
-(slime-setup)
+(require 'slime-autoloads)
+(slime-setup '(slime-fancy slime-sprof))
 
 (require 'evil)
 (evil-mode 1)
