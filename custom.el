41ff94ec126957e4c0278d04b7e94f8923f7dfcd
Simon Hafner <hafnersimon@gmail.com>
2012-08-29 00:50:37 -0500

    + custom.el

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(browse-url-browser-function (quote browse-url-chromium))
+ '(ido-default-file-method (quote raise-frame))
+ '(nxml-slash-auto-complete-flag t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(region ((t (:background "gold" :foreground "gtk_selection_fg_color")))))
--- init.el
+++ init.el
@@ -166,12 +166,20 @@
 (load "auctex.el" nil t t)
 (load "preview-latex.el" nil t t)
 
+(require 'saveplace)
+(setq-default save-place t)
+(setq save-place-file "~/.emacs.d/saved-places")
+
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(load custom-file)
 ;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(browse-url-browser-function (quote browse-url-chromium))
+ '(ido-default-file-method (quote raise-frame))
  '(nxml-slash-auto-complete-flag t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
