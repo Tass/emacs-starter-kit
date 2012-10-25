af59fc8188490c714fb1f6246e5758d0e33f6c86
Simon Hafner <hafnersimon@gmail.com>
2012-10-24 21:37:22 -0500

    added \\ escapes

--- vendor/assorted/org-babel-qtree.el
+++ vendor/assorted/org-babel-qtree.el
@@ -5,27 +5,36 @@
 
 (defun org-babel-execute:qtree (body params)
   "Reformat a block of lisp-edited tree to one tikz-qtree likes."
-  (let (( tree
+  (let ((tree
           (concat "\\begin{tikzpicture}
 \\tikzset{every tree node/.style={align=center, anchor=north}}
-\\Tree "
-                  (replace-regexp-in-string
-                   " \\_<\\w+\\_>" (lambda (x) (concat "\\\\\\\\" (substring x 1))) 
-                   (replace-regexp-in-string
-                    (regexp-quote "]") " ]" ; qtree needs a space
-                                        ; before every closing
-                                        ; bracket.
-                    (replace-regexp-in-string
-                     (regexp-quote "[]") "[.{}]" body)) ; empty leaf
-                                        ; nodes, see
-                                        ; http://tex.stackexchange.com/questions/75915
-                   )                    ; For
-                                        ; http://tex.stackexchange.com/questions/75217
+\\Tree " (org-babel:qtree-process-string body)
                   "\n\\end{tikzpicture}"
                   )))
     (if (assoc :landscape params)
         (concat "\\begin{landscape}\n" tree "\n\\end{landscape}")
       tree)))
+
+(defun org-babel:qtree-process-string (string)
+  (with-output-to-string
+    (loop for line in (split-string string "\n")
+          if (not (string-match "^[\\s\\t]*\\\\" line))
+          do (princ (concat (replace-regexp-in-string
+                             " \\_<\\w+\\_>" (lambda (x) (concat "\\\\\\\\" (substring x 1))) 
+                             (replace-regexp-in-string
+                              (regexp-quote "]") " ]"             ; qtree needs a space
+                                        ; before every closing
+                                        ; bracket.
+                              (replace-regexp-in-string
+                               (regexp-quote "[]") "[.{}]" line)) ; empty leaf
+                                        ; nodes, see
+                                        ; http://tex.stackexchange.com/questions/75915
+                             )
+                                        ; http://tex.stackexchange.com/questions/75217
+                     "\n"))
+          else do (princ (concat line "\n"))
+                    ))
+  )
 
 (setq org-babel-default-header-args:qtree '((:results . "latex") (:exports . "results")))
 (add-to-list 'org-src-lang-modes '("qtree" . qtree))
