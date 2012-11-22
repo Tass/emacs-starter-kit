6529f131229c2f15097152dc2602112b1fabf7dc
Simon Hafner <hafnersimon@gmail.com>
2012-11-22 14:49:07 -0600

    colorthemed

--- custom.el
+++ custom.el
@@ -4,8 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(browse-url-browser-function (quote browse-url-chromium))
- '(ido-default-file-method (quote raise-frame))
- '(nxml-slash-auto-complete-flag t))
+ '(custom-safe-themes (quote ("501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" default)))
+ '(nxml-slash-auto-complete-flag t)
+ '(send-mail-function nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -172,18 +172,9 @@
 
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file)
+
+(require 'custom-color)
+
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(browse-url-browser-function (quote browse-url-chromium))
- '(ido-default-file-method (quote raise-frame))
- '(nxml-slash-auto-complete-flag t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(region ((t (:background "gold" :foreground "gtk_selection_fg_color")))))
+(put 'narrow-to-region 'disabled nil)
+
