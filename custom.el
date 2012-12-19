082ffac06baf11a60bcb5a90da9a8235f07f0397
Simon Hafner <hafnersimon@gmail.com>
2012-12-19 15:11:25 -0600

    customization

--- custom.el
+++ custom.el
@@ -4,8 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(browse-url-browser-function (quote browse-url-chromium))
- '(custom-safe-themes (quote ("501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" default)))
+ '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" default)))
  '(nxml-slash-auto-complete-flag t)
+ '(org-export-latex-inline-image-extensions (quote ("pdf" "jpeg" "jpg" "png" "ps" "eps" "svg")))
  '(send-mail-function nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
