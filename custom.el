ad0c95ea493d4a7a23579ab4164b862035247cf8
Simon Hafner <hafnersimon@gmail.com>
2013-02-11 12:38:01 -0600

    evil-shift-width set to two

--- custom.el
+++ custom.el
@@ -5,6 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(browse-url-browser-function (quote browse-url-chromium))
  '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" default)))
+ '(evil-shift-width 2)
  '(nxml-slash-auto-complete-flag t)
  '(org-export-latex-inline-image-extensions (quote ("pdf" "jpeg" "jpg" "png" "ps" "eps" "svg")))
  '(send-mail-function nil))
