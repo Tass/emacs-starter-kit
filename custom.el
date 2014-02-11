5a5e9c2f04622642c8e2ae6eeefe1daec18168f8
Simon Hafner <hafnersimon@gmail.com>
2014-02-11 16:16:18 -0600

    updated custom.el

--- custom.el
+++ custom.el
@@ -6,6 +6,7 @@
  '(browse-url-browser-function (quote browse-url-chromium))
  '(custom-safe-themes (quote ("9226bb420f414235005f1977615fd93cfbf486648c98c87f4c22f300e03f00a0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" default)))
  '(evil-shift-width 2)
+ '(glasses-uncapitalize-p t)
  '(nimrod-command "/home/tass/dev/nimrod/Nimrod/bin/nimrod")
  '(nxml-slash-auto-complete-flag t)
  '(org-export-latex-inline-image-extensions (quote ("pdf" "jpeg" "jpg" "png" "ps" "eps" "svg")))
