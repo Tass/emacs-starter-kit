9ddd5003bd2cdc26437e973025976f657d02836b
Simon Hafner <hafnersimon@gmail.com>
2014-02-14 13:50:52 -0600

    Revert "it's called org-export-latex-default-packages"
    This reverts commit b4efd272551ec40efb665173e87994ef835995e1.

--- xetex.el
+++ xetex.el
@@ -1,11 +1,11 @@
 ;;; XeLaTeX customisations
 ;; remove "inputenc" from default packages as it clashes with xelatex
-(setf org-export-latex-default-packages-alist
+(setf org-latex-default-packages-alist
       (remove '("AUTO" "inputenc" t) org-latex-default-packages-alist))
 
-(add-to-list 'org-export-latex-packages-alist '("" "xltxtra" t))
+(add-to-list 'org-latex-packages-alist '("" "xltxtra" t))
 ;; choose Linux Libertine O as serif and Linux Biolinum O as sans-serif fonts
-(add-to-list 'org-export-latex-packages-alist '("" "libertineotf" t))
+(add-to-list 'org-latex-packages-alist '("" "libertineotf" t))
 
 ;; org to latex customisations, -shell-escape needed for minted
 (setq org-export-dispatch-use-expert-ui t ; non-intrusive export dispatch
